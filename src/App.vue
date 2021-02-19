aedf3a7b4ee49535ec3a00e62a734c0afbac1851
bradleyaellis <bradleyaellis@gmail.com>
2021-02-19 13:12:04 -0600

    lining up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
     v-app
       v-app-bar(app color="success" dark)
-        v-row.flex-row.px-auto
+        v-row.flex-row.px-auto(v-if="!$vuetify.breakpoint.xs")
           v-col.d-flex.flex-column.justify-start.mt-2(cols="4")
             title.d-flex.align-start Recipes Lite
             caption.d-flex.align-end.caption(v-if="!$vuetify.breakpoint.xs") No blog, no fuss. Recipes done right. Recipes Lite
@@ -9,6 +9,14 @@
             Search
           v-col.d-flex.justify-end(cols="4")
             Random
+        v-row.flex-row.px-auto(v-else)
+          v-col.d-flex.flex-column.mt-3(cols="3")
+            header.d-flex.justify-center Recipes Light
+          v-col.d-flex.align-left.py-0(cols="7")
+            Search
+          v-col.d-flex.justify-end.pr-0(cols="2")
+            Random
+
       v-main
         router-view(:key="$route.FullPath")
 </template>
